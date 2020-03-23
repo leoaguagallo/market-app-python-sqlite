34fa707d7d56128eea7563d0d018f1f7b83467da
Moyses Aguagallo <leo.aigaje12@gmail.com>
2020-03-22 22:30:06 -0500

    Form with tables

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ class Product:
         frame.grid(row=0, column=0, columnspan=3, pady=20) #posicionamiento
         
         #Name Input  
-        Label(frame, text='Name: ').grid(row = 1, column = 0)
-        self.name = Entry(frame)
+        Label(frame, text='Name: ').grid(row = 1, column = 0) #creacion
+        self.name = Entry(frame) #add to frame
         self.name.focus() #focus en name
-        self.name.grid(row = 1, column = 1)
+        self.name.grid(row = 1, column = 1) #posicion
 
         #Price input
         Label(frame, text='Price: ').grid(row = 2, column = 0)
@@ -29,6 +29,12 @@ class Product:
 
         #Button add product
         ttk.Button(frame, text = 'Save Product').grid(row = 3, columnspan = 2, sticky =  W + E)
+
+        #table on frame
+        self.table_model = ttk.Treeview(height = 10, columns = 2) #creacion
+        self.table_model.grid(row = 4, column = 0, columnspan = 2) #posicion
+        self.table_model.heading('#0', text='Name', anchor=CENTER) #titulo
+        self.table_model.heading('#1', text='Price', anchor=CENTER)
 
 
 
